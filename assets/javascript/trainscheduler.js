8ff2dc5a43d8be4abf6eab44cb491065cf57fbf2
Aziz Ahmed <m.aziz.ahmed@gmail.com>
2016-09-05 16:04:31 -0400

    fixed

--- assets/javascript/trainscheduler.js
+++ assets/javascript/trainscheduler.js
@@ -1,25 +1,28 @@
-<script src="https://www.gstatic.com/firebasejs/3.3.0/firebase.js"></script>
-<script>
-  // Initialize Firebase
-  var config = {
-    apiKey: "AIzaSyDrKWSJmT8ppT87DpEPhDE3mJODW2X90pg",
-    authDomain: "test-e8c2b.firebaseapp.com",
-    databaseURL: "https://test-e8c2b.firebaseio.com",
-    storageBucket: "test-e8c2b.appspot.com",
-  };
-  firebase.initializeApp(config);
-</script>
+	  // Initialize Firebase
+	  var config = {
+	    apiKey: "AIzaSyBUJRnnzmJrHBqNwGjhtaHXWhaQstBS3us",
+	    authDomain: "trainscheduler-924b1.firebaseapp.com",
+	    databaseURL: "https://trainscheduler-924b1.firebaseio.com",
+	    storageBucket: "",
+	  };
+	  firebase.initializeApp(config);
+
+var database = firebase.database();
+
+$(document).ready(function(){
 
 
 // submit info button, collect data
 $('#Submit').on('click', function(){
 
-		trainName = $('#name').val().trim();
-		destination = $('#destination').val().trim();
-		frequency = $('#freq').val().trim();
-		uTc = $('#time').val().trim();
+		var trainName = $('#name').val().trim();
+		var destination = $('#destination').val().trim();
+		var frequency = $('#freq').val().trim();
+		var uTc = moment($('#time').val().trim(), "hh:mm").subtract(10, "years").format("X");
 
-		DatabaseURL.push({
+		database.ref().push(
+
+		{
 			name: trainName,
 			place: destination,
 			frequency: frequency,
@@ -30,13 +33,42 @@ $('#Submit').on('click', function(){
 		$('#freq').val('');
 		$('#time').val('');
 
-		return false;
+	// empty();
+	return false;
 
 });
 
+	
+});
 // need to add all the train data to database
 
 //DataBaseURL.on('child_added', function(childSnapshot){
 //    		var table = $('#table');
 //            var row = $('<td>');
-    
+
+database.ref().on('child_added',function(snapshot){
+        //Time input of the frequency of train.
+        var frequency = snapshot.val().frequency;
+        //live time is real time.
+        var liveTime = moment();
+        var firstTrain = moment(liveTime);
+        //one month back to avoid live time.
+        var oneMonth = moment(snapshot.val().firstTrain, 'hh:mm').subtract(1, 'month');
+        var timeOftrain = moment(oneMonth).format('HH:mm:ss');
+        //another one, to track the time of the train by the time put on Input.
+        var anotherMonth = moment(timeOftrain,'hh:mm' ).subtract(1, 'month');
+        
+        var residualTime = moment().diff(moment(oneMonth), 'minutes');
+        //Time apart between trains.
+        var timeRemainder = residualTime % frequency;
+        //math equation to solve the minutes till the next train.
+        var whereIstheTrain = frequency - timeRemainder; 
+        //next train arrival 
+        var nextarrival = moment().add(nextarrival, 'minutes').format('HH:mm:ss');
+        //Appending the child_add database into a tbody table when the name is inserted in the input.
+        $('#theTimeSchedule > tbody').append('<tr><td>' + snapshot.val().trainName + '</td><td>' +
+            snapshot.val().destination + '</td><td>' + snapshot.val().frequency + " minutes" + '</td><td>' +
+            nextarrival + '</td><td>' + whereIstheTrain + " minutes" + '</td></tr>');
+    });
+
+
